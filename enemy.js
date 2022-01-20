0737ed80bd296b4f54a5cebb88cd367daeb1fcc1
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-01-20 11:17:32 -0600

    Update enemy.js

--- enemy.js
+++ enemy.js
@@ -3,8 +3,24 @@ function Enemy(x,y,w,h){
   this.y = y;
   this.w = w;
   this.h = h;
+  this.direction = "r"
   
   this.display = function(){
     rect(this.x, this.y, this.w, this.h);
   }//end function display
+  
+  this.update= function(){
+    if(this.direction == "d"){
+      this.y += 3;
+    }
+    if(this.direction == "u"){
+      this.y -= 3;
+    }
+    if(this.direction == "l"){
+      this.x -= 3;
+    }
+    if(this.direction == "r"){
+      this.x += 3;
+    }
+  }
 }//end Enemy
