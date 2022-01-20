ee762cbb80529213ceb2c516d6d64c89f41eed18
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-01-20 11:36:37 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,25 @@
 let e;
+let l;
+let r;
 
 function setup() {
   createCanvas(400, 400);
   e = new Enemy(200,50,50,50);
+  r = new RightTurn(200,200,60,60);
+  l = new LeftTurn(300,200,60,60);
 }
 
 function draw() {
   background(220);
   e.display();
   e.update();
+  r.display();
+  l.display();
+  checkForTurns();
+}
+
+//check to see if we collide with
+//a leftTurn, or rightTurn
+function checkForTurns(){
+  
 }
