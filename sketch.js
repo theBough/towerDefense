f318c15d893117a40c26bc272db52f65b404e40c
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-01-25 11:02:03 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,15 +5,15 @@ let r;
 function setup() {
   createCanvas(400, 400);
   e = new Enemy(200,50,50,50);
-  r = new RightTurn(200,200,60,60);
-  l = new LeftTurn(300,200,60,60);
+  //r = new RightTurn(200,200,60,60);
+  l = new LeftTurn(200,200,60,60);
 }
 
 function draw() {
   background(220);
   e.display();
   e.update();
-  r.display();
+ // r.display();
   l.display();
   checkForTurns();
 }
@@ -21,5 +21,7 @@ function draw() {
 //check to see if we collide with
 //a leftTurn, or rightTurn
 function checkForTurns(){
-  
+  if((Math.abs(e.x - l.x) < Math.abs(e.w/2 + l.w/2)) && (Math.abs(e.y - l.y) < Math.abs(e.h/2 + l.h/2)) ){
+    console.log("Time to Turn")
+  }
 }
