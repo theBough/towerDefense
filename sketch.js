7419a937da81ef8c3eaf7d0440b169091ce1194e
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-01-27 11:39:53 -0600

    Update sketch.js
    If you have made your route.
    Just copy "send Enemy" function
    and CheckForTurns

--- sketch.js
+++ sketch.js
@@ -1,35 +1,40 @@
-let e;
+let e = [];
 let l = [];
 let r = [];
 let img;
 
 function setup() {
   createCanvas(600, 400);
-  e = new Enemy(300, 0, 25, 25);
-  placeTurnBlocks()
-  
-  img= loadImage("map.jpg")
+
+  placeTurnBlocks();
+
+  img = loadImage("map.jpg");
+  sendEnemy();
 }
-function placeTurnBlocks(){
+function sendEnemy() {
+  e.push(new Enemy(300, 0, 30, 50));
+}
+function placeTurnBlocks() {
   //create a block for every corner.
   //reminder: RightTurn(x location, y location, width, height)
-  r.push(new RightTurn(500,65,40,40,"red"));
-  l.push(new LeftTurn(295, 85, 40, 40,"blue"));
+  r.push(new RightTurn(500, 65, 40, 40, "red"));
+  l.push(new LeftTurn(295, 85, 40, 40, "blue"));
 }
 
 function draw() {
   background(220);
-  image(img,0,0,600,400)
-  for(i = 0; i<r.length;i++){
+  image(img, 0, 0, 600, 400);
+  for (i = 0; i < r.length; i++) {
     r[i].display();
   }
-   for(i = 0; i<l.length;i++){
+  for (i = 0; i < l.length; i++) {
     l[i].display();
   }
-  
-  
-  e.display();
-  e.update();
+  for (i = 0; i < e.length; i++) {
+    e[i].display();
+    e[i].update();
+  }
+
   checkForTurns();
 }
 
@@ -53,7 +58,7 @@ function changeDirection(thisEnemy, thisTurn) {
         thisEnemy.direction = "r";
         break;
     } //end Switch
-  }else{
+  } else {
     switch (thisEnemy.direction) {
       case "l":
         thisEnemy.x += 3;
@@ -78,23 +83,24 @@ function changeDirection(thisEnemy, thisTurn) {
 //check to see if we collide with
 //a leftTurn, or rightTurn
 function checkForTurns() {
-  rectMode(CENTER)
-  for(i = 0; i< l.length; i++){
-    if (
-    Math.abs(e.x - l[i].x) < Math.abs(e.w / 2 + l[i].w / 2) &&
-    Math.abs(e.y - l[i].y) < Math.abs(e.h / 2 + l[i].h / 2)
-  ) {
-    changeDirection(e,l[i]);
-    }//end if
-  }//end for
-  
-  for( i = 0 ; i < r.length; i++){
-    if (
-    Math.abs(e.x - r[i].x) < Math.abs(e.w / 2 + r[i].w / 2) &&
-    Math.abs(e.y - r[i].y) < Math.abs(e.h / 2 + r[i].h / 2)
-  ) {
-    changeDirection(e,r[i]);
-  }
-  }
-  
+  rectMode(CENTER);
+  for (j = 0; j < e.length; j++) {
+    for (i = 0; i < l.length; i++) {
+      if (
+        Math.abs(e[j].x - l[i].x) < Math.abs(e[j].w / 2 + l[i].w / 2) &&
+        Math.abs(e[j].y - l[i].y) < Math.abs(e[j].h / 2 + l[i].h / 2)
+      ) {
+        changeDirection(e[j], l[i]);
+      } //end if
+    } //end for
+
+    for (i = 0; i < r.length; i++) {
+      if (
+        Math.abs(e[j].x - r[i].x) < Math.abs(e[j].w / 2 + r[i].w / 2) &&
+        Math.abs(e[j].y - r[i].y) < Math.abs(e[j].h / 2 + r[i].h / 2)
+      ) {
+        changeDirection(e[j], r[i]);
+      } //end if
+    } //end for i
+  } //end for j
 }
